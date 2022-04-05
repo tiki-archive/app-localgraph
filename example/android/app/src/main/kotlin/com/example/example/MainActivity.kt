5833eb07f2a2558e6dcd2e3cfe1e4ce62a307d40
Mike Audi <mike@mytiki.com>
2022-04-05 14:24:07 -0500

    initialize template repo
    fixes #1

---
+++ example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mytiki.localgraph.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example/integration_test/localgraph_test.dart
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) TIKI Inc.
+ * MIT license. See LICENSE file in root directory.
+ */
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:localgraph/localgraph.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('localgraph Tests', () {});
+}
